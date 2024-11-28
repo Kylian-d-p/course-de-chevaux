5b820ac9be9a88011b5ba59b6b0fc18944a4b421
Kylian Dupouy Peron <kdperon@yahoo.com>
2024-11-28 11:43:17 +0100

    refactor betting logic to use event listener and improve player handling

--- static/spectate/index.js
+++ static/spectate/index.js
@@ -1,28 +1,51 @@
 import { renderGame } from "../js/render-game.js";
 
 const searchParams = new URLSearchParams(window.location.search);
-
 const gameId = searchParams.get("id");
-const WAINTING_TEXT = "Faites vos paris maintenant !"
-
+const WAINTING_TEXT = "Faites vos paris maintenant !";
 
 if (typeof gameId !== "string") {
   document.location.replace(`/index.html`);
 } else {
   const socket = io();
+
   let gameStatus = "stopped";
-  let players = []
+  let players = [];
+  
+  const betEventListener = (e) => {
+    const i = Array.prototype.indexOf.call(e.target.parentElement.children, e.target);
+    
+    if (gameStatus !== "running" && players.length >= i + 1) {
+      socket.emit("bet coins", {
+        gameId: gameId,
+        amount: 10,
+        playerId: players[i].id,
+      });
+    }
+  };
+  
   socket.emit("request room spectate access", {
     id: gameId,
   });
 
   socket.on("players update", (newPlayers) => {
-    players = newPlayers
+    players = newPlayers;
     renderGame(players, gameStatus, WAINTING_TEXT);
+
+    const bets = document.querySelectorAll(".bet");
+    bets.forEach((bet) => {
+      bet.removeEventListener("click", betEventListener);
+    });
+
+    if (gameStatus !== "running") {
+      bets.forEach((bet) => {
+        bet.addEventListener("click", betEventListener);
+      });
+    }
   });
 
   socket.on("game status", ({ status: newStatus }) => {
-    gameStatus = newStatus
+    gameStatus = newStatus;
     renderGame(players, gameStatus, WAINTING_TEXT);
   });
 
@@ -31,20 +54,6 @@ if (typeof gameId !== "string") {
     if (data.needAuth) {
       document.location.href = `/login/index.html?redirectTo=${encodeURIComponent(document.location.href)}`;
     }
-  });
-
-  const bets = document.querySelectorAll(".bet");
-
-  bets.forEach((bet, i) => {
-    bet.addEventListener("click", () => {
-      if (gameStatus !== "running" && players.length > i+1) {
-        socket.emit("bet coins", {
-          gameId: gameId,
-          amount: 10,
-          playerId: players[i].id,
-        });
-      }
-    });
   });
 
   let totalCoins = 0;
