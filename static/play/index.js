b19f28a79d25eb5c4eba07e59175c674b9cbe4e1
alexis-da <alexis.dacruz40@gmail.com>
2024-11-28 11:43:51 +0100

    boutton pour copier le code de la partie

--- static/play/index.js
+++ static/play/index.js
@@ -1,6 +1,6 @@
 import { renderGame } from "../js/render-game.js";
 
-const WAINTING_TEXT = "Appuyer sur espace pour commencer"
+const WAINTING_TEXT = "Appuyer sur espace pour commencer";
 const searchParams = new URLSearchParams(window.location.search);
 
 const gameId = searchParams.get("id");
@@ -10,35 +10,47 @@ if (typeof gameId !== "string") {
     res.json().then((data) => {
       if (!res.ok) {
         data.message && alert(data.message);
-        return document.location.replace(`/login/index.html?redirectTo=${encodeURIComponent(window.location.href)}`);
+        return document.location.replace(
+          `/login/index.html?redirectTo=${encodeURIComponent(
+            window.location.href
+          )}`
+        );
       }
-      document.location.replace(`${document.location.pathname}?id=${data.data.gameId}`);
+      document.location.replace(
+        `${document.location.pathname}?id=${data.data.gameId}`
+      );
     });
   });
 } else {
+  const codeButton = document.querySelector("#code-game");
+  codeButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(gameId);
+  });
+
   const socket = io();
   let gameStatus = "stopped";
-  let players = []
+  let players = [];
   socket.emit("request room play access", {
     id: gameId,
   });
 
   socket.on("players update", (newPlayers) => {
-    players = newPlayers
+    players = newPlayers;
     renderGame(players, gameStatus, WAINTING_TEXT);
     console.log(gameStatus);
-    
   });
 
-  socket.on("game status", ({ status:newStatus }) => {
-    gameStatus = newStatus
+  socket.on("game status", ({ status: newStatus }) => {
+    gameStatus = newStatus;
     renderGame(players, gameStatus, WAINTING_TEXT);
   });
 
   socket.on("info", (data) => {
     alert(data.message);
     if (data.needAuth) {
-      document.location.href = `/login/index.html?redirectTo=${encodeURIComponent(document.location.href)}`;
+      document.location.href = `/login/index.html?redirectTo=${encodeURIComponent(
+        document.location.href
+      )}`;
     }
   });
 
@@ -46,7 +58,11 @@ if (typeof gameId !== "string") {
     if (e.key === " ") {
       socket.emit("add progress", { gameId });
       if (gameStatus === "stopped") {
-        fetch("/api/room/run", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ gameId }) });
+        fetch("/api/room/run", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ gameId }),
+        });
       }
     }
   });
